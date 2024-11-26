eea4c19f0375424600685006347099b7629994e5
musire <mcmusire@gmail.com>
2024-11-26 17:54:21 -0600

    installed moment timezone and created a util function

---
+++ helper/moment.js
@@ -0,0 +1,9 @@
+const moment = require('moment-timezone');
+
+const getTimestamp = () => {
+    const timezone = "America/New_York";
+    const localTimestamp = moment.tz(Date.now(), timezone).valueOf()
+    return localTimestamp
+};
+
+module.exports = getTimestamp;
